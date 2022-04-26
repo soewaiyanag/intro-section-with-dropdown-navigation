45e28854a22bf6819cfe30ac5cdb611198b05419
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2022-04-26 14:08:53 +0630

    add hero image for mobile

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Texts from "./Texts";
+import heroMobile from "@images/image-hero-mobile.png";
+import heroDesktop from "@images/image-hero-desktop.png";
 
 const Main = () => {
   return (
-    <main className="container">
+    <main className="sm:container flex flex-col gap-8">
+      <img className="sm:hidden" src={heroMobile} alt="hero image" />
+      <img className="hidden sm:block" src={heroDesktop} alt="hero image" />
       <Texts />
     </main>
   );
